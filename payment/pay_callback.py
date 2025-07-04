a10fbb21f60359aca5ef7d81a69a06ae9893f407
FathullaLatipov <fathullaxonlatipov@gmail.com>
2025-07-04 23:42:59 +0500

    finish14

--- payment/pay_callback.py
+++ payment/pay_callback.py
@@ -275,8 +275,8 @@ class PaymeCallbackView(PaymeWebHookAPIView):
 
             # 🔐 Определяем offer_code и группу
             amount = int(transaction.amount)  # в тийинах
-            if amount == 1200:
-                offer_code = "fitpack_course_standart"
+            if amount == 1000:
+                offer_code = "fitpack_course_test"
                 group_id = 4312537
             elif amount == 1999000:
                 offer_code = "fitpack_course_plus"
