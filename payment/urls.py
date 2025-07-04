51a03ef27806c370d4a9ca076b9534de2287782c
FathullaLatipov <fathullaxonlatipov@gmail.com>
2025-07-04 19:59:47 +0500

    changed5

--- payment/urls.py
+++ payment/urls.py
@@ -2,9 +2,10 @@
 from django.urls import path
 
 from payment.pay_link import GeneratePayLinkAPIView
-from payment.pay_callback import PaymeCallbackView
+from payment.pay_callback import PaymeCallbackView, GetCourseWebhookView
 
 urlpatterns = [
     path('pay-link/', GeneratePayLinkAPIView.as_view(), name='generate-pay-link'),
     path('payme/callback/', PaymeCallbackView.as_view(), name='payme-callback'),
+    path("getcourse/webhook/", GetCourseWebhookView.as_view(), name="getcourse-webhook"),
 ]
